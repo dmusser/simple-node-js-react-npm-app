8b8b7dc24d55df210e271a99be8b61cbe95ae53a
dmusser <dmusser@dmusser.com>
2021-02-28 18:45:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-const body= nearestBody(node);
+import registerServiceWorker from './registerServiceWorker';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
